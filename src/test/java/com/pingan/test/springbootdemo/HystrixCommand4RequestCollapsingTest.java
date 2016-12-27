28d8bf4044c271558278b92b57852713ea315259
heliuxing902 <heliuxing902@pingan.com.cn>
2016-12-27 19:24:23 +0800

    upd collapsing

--- src/test/java/com/pingan/test/springbootdemo/HystrixCommand4RequestCollapsingTest.java
+++ src/test/java/com/pingan/test/springbootdemo/HystrixCommand4RequestCollapsingTest.java
@@ -26,23 +26,22 @@ import com.pingan.test.springbootdemo.hystrix.HelloWorldHystrixCommand;
  * 
  */
 public class HystrixCommand4RequestCollapsingTest {
-//        @Test
+        @Test
         public void testCollapser() throws Exception {
             HystrixRequestContext context = HystrixRequestContext.initializeContext();
             try {
             	
                 Future<String> f1 = new HelloWorldHystrixCollapser(1).queue();
                 Future<String> f2 = new HelloWorldHystrixCollapser(2).queue();
-                System.out.println(new HelloWorldHystrixCollapser(1).execute());	// 这条很可能会合并到f1和f2的批量请求中
-                System.out.println(new HelloWorldHystrixCollapser(1).execute());	// 由于上面有IO打印，这条很可能不会合并到f1和f2的批量请求中
-                Future<String> f5 = new HelloWorldHystrixCommand("5").queue();
-                Future<String> f6 = new HelloWorldHystrixCommand("6").queue();
-                
-                // f3和f4，如果sleep时间够小则会合并，如果sleep时间够大则不会合并，默认10ms
+//                System.out.println(new HelloWorldHystrixCollapser(1).execute());	// 这条很可能会合并到f1和f2的批量请求中
+//                System.out.println(new HelloWorldHystrixCollapser(1).execute());	// 由于上面有IO打印，这条很可能不会合并到f1和f2的批量请求中
                 Future<String> f3 = new HelloWorldHystrixCollapser(3).queue();
-                TimeUnit.MILLISECONDS.sleep(13);
                 Future<String> f4 = new HelloWorldHystrixCollapser(4).queue();
-                
+
+                Future<String> f5 = new HelloWorldHystrixCollapser(5).queue();
+                // f5和f6，如果sleep时间够小则会合并，如果sleep时间够大则不会合并，默认10ms
+                TimeUnit.MILLISECONDS.sleep(13);
+                Future<String> f6 = new HelloWorldHystrixCollapser(6).queue();              
           
                 System.out.println(f1.get());
                 System.out.println(f2.get());
@@ -51,9 +50,9 @@ public class HystrixCommand4RequestCollapsingTest {
                 System.out.println(f5.get());
                 System.out.println(f6.get());
                 // 下面3条都不在一个批量请求中
-                System.out.println(new HelloWorldHystrixCollapser(7).execute());
-                System.out.println(new HelloWorldHystrixCollapser(8).queue().get());
-                System.out.println(new HelloWorldHystrixCollapser(9).queue().get());
+//                System.out.println(new HelloWorldHystrixCollapser(7).execute());
+//                System.out.println(new HelloWorldHystrixCollapser(8).queue().get());
+//                System.out.println(new HelloWorldHystrixCollapser(9).queue().get());
 
                 // note：numExecuted表示共有几个命令执行，1个批量多命令请求算一个，这个实际值可能比代码写的要多，因为due to non-determinism of scheduler since this example uses the real timer
                 int numExecuted = HystrixRequestLog.getCurrentRequest().getAllExecutedCommands().size();
